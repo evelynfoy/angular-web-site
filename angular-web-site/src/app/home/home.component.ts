fcaf8c646a371f63c222d106ec6a5fd905c0f956
Evelyn Foy <evelyn.foy@hotmail.com>
2024-02-22 08:22:21 +0000

    Add pimages to home page

--- angular-web-site/src/app/home/home.component.ts
+++ angular-web-site/src/app/home/home.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
