655fcbc8ac46e6140c1e401dbd0795c28751798a
Evelyn Foy <evelyn.foy@hotmail.com>
2024-03-03 17:46:33 +0000

    Add a guard for the add projects page

---
+++ angular-web-site/src/app/auth/auth.guard..ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { Observable, map, take } from "rxjs";
+import { AuthService } from "./auth.service";
+
+@Injectable({ providedIn:"root"})
+export class AuthGuard implements CanActivate {
+
+    constructor(private authService: AuthService, private router:Router ) {}
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+        
+        return this.authService.loggedIn.pipe(
+            take (1),
+            map(loggedIn => {
+                if (loggedIn) {
+                    return true
+                }
+                return this.router.createUrlTree(['/auth'])
+            })
+        )
+    }
+
+}
